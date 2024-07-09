c074b076a8877c03bd2a963c23a18b3436a20972
Douglas Sellers <doug@flossy.com>
2024-07-08 23:00:15 -0700

    Re-wired everything

--- main.py
+++ main.py
@@ -10,38 +10,28 @@ import time
 if os.getenv('LOAD_ENV') is not None:
     load_dotenv()
 def main():
-    # Initialize GitHub API with token
     code_vector_store = VectorStore( 'ai-school-tech-writer-code')
     code_vector_store.add_vector( CodeBaseVector('.').vectorize() )
     time.sleep(5)
-    print( code_base_summary( code_vector_store ) )
+    project_summary =  code_base_summary( code_vector_store )
     code_vector_store.clear_index()
 
-    exit(0)
     g = Github(os.getenv('GITHUB_TOKEN'))
-
-    # Get the repo path and PR number from the environment variables
     repo_path = os.getenv('REPO_PATH')
     pull_request_number = int(os.getenv('PR_NUMBER'))
-
-    # Get the repo object
     repo = g.get_repo(repo_path)
-    # Fetch README content (assuming README.md)
-    readme_content = repo.get_contents("README.md")
-    
-    # print(readme_content)
-    # Fetch pull request by number
     pull_request = repo.get_pull(pull_request_number)
-
     vector = PullRequestVector(pull_request, pull_request_number)
     changes_vector_store = VectorStore( os.getenv( 'PINECONE_INDEX_CHANGES'))
 
-#    changes_vector_store.add_vector(vector.vectorize())
+    changes_vector_store.add_vector(vector.vectorize())
 
-    updated_readme = last_five_pr_summary( pull_request_number, changes_vector_store )
-    print(updated_readme)
+    pr_summary = last_five_pr_summary( pull_request_number, changes_vector_store )
+    readme_content = repo.get_contents("README.md")
 
-    #update_readme_and_create_pr(repo, updated_readme, readme_content.sha)
+    updated_readme = f"# AI for Developer Productivity: Technical Writer Agent\n##Project Summary\n{project_summary}\n\n##Recent Changes\n{pr_summary}"
+    print( updated_readme )
+    update_readme_and_create_pr(repo, updated_readme, readme_content.sha)
 
 if __name__ == '__main__':
     main()
